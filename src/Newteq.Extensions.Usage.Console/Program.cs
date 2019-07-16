3dea3038bc4c6dea8b102d341daea893bd7d0194
Newteq <newt.fourie@entelect.co.za>
2019-07-16 14:31:43 +0200

    Create a working example of the string extensions

--- src/Newteq.Extensions.Usage.Console/Program.cs
+++ src/Newteq.Extensions.Usage.Console/Program.cs
@@ -6,7 +6,35 @@ namespace Newteq.Extensions.Usage.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var helloWorld = "hello world with all the first letter's capitalized";
+            Console.WriteLine(helloWorld.ToTitle());
+
+            NextExample();
+
+            var names = new string[]
+            {
+                "John",
+                "Dave",
+                "Dane",
+                "Alice",
+                "Sarah",
+                "Sofia",
+            };
+            Console.WriteLine($"Everyone is here: {names.NaturalJoin()}");
+
+            PromptClose();
+        }
+
+        private static void NextExample()
+        {
+            Console.WriteLine("Press any key for next example");
+            Console.ReadLine();
+        }
+
+        private static void PromptClose()
+        {
+            Console.WriteLine("That's it :). Press any key for to exit");
+            Console.ReadLine();
         }
     }
 }
